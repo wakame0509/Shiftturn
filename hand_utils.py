b6c6b1f76b130d173fa2a5b08c3cdd3a8ee286b4
wakame0509 <wakamegek1699@yahoo.ne.jp>
2025-06-07 20:04:47 +0900

    Update hand_utils.py

--- hand_utils.py
+++ hand_utils.py
@@ -1,31 +1,24 @@
 # hand_utils.py
 
-# 169通りのスターティングハンド
+# 169通りのスターティングハンドを定義（"AKs", "AQo", など）
 all_starting_hands = [
-    "AA", "KK", "QQ", "JJ", "TT", "99", "88", "77", "66", "55", "44", "33", "22",
-    "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s",
-    "KQs", "KJs", "KTs", "K9s", "K8s", "K7s", "K6s", "K5s", "K4s", "K3s", "K2s",
-    "QJs", "QTs", "Q9s", "Q8s", "Q7s", "Q6s", "Q5s", "Q4s", "Q3s", "Q2s",
-    "JTs", "J9s", "J8s", "J7s", "J6s", "J5s", "J4s", "J3s", "J2s",
-    "T9s", "T8s", "T7s", "T6s", "T5s", "T4s", "T3s", "T2s",
-    "98s", "97s", "96s", "95s", "94s", "93s", "92s",
-    "87s", "86s", "85s", "84s", "83s", "82s",
-    "76s", "75s", "74s", "73s", "72s",
-    "65s", "64s", "63s", "62s",
-    "54s", "53s", "52s",
-    "43s", "42s",
-    "32s",
-
-    "AKo", "AQo", "AJo", "ATo", "A9o", "A8o", "A7o", "A6o", "A5o", "A4o", "A3o", "A2o",
-    "KQo", "KJo", "KTo", "K9o", "K8o", "K7o", "K6o", "K5o", "K4o", "K3o", "K2o",
-    "QJo", "QTo", "Q9o", "Q8o", "Q7o", "Q6o", "Q5o", "Q4o", "Q3o", "Q2o",
-    "JTo", "J9o", "J8o", "J7o", "J6o", "J5o", "J4o", "J3o", "J2o",
-    "T9o", "T8o", "T7o", "T6o", "T5o", "T4o", "T3o", "T2o",
-    "98o", "97o", "96o", "95o", "94o", "93o", "92o",
-    "87o", "86o", "85o", "84o", "83o", "82o",
-    "76o", "75o", "74o", "73o", "72o",
-    "65o", "64o", "63o", "62o",
-    "54o", "53o", "52o",
-    "43o", "42o",
-    "32o"
+    f"{r1}{r2}s" if i < j else f"{r2}{r1}o" if i > j else f"{r1}{r2}"
+    for i, r1 in enumerate("AKQJT98765432")
+    for j, r2 in enumerate("AKQJT98765432")
+    if i <= j
 ]
+
+# 例: "AKs" → ["Ah", "Kh"]
+def convert_hand_to_cards(hand_notation):
+    ranks = "AKQJT98765432"
+    suits = ['h', 'd', 'c', 's']
+    r1, r2 = hand_notation[0], hand_notation[1]
+    suited = hand_notation.endswith('s')
+    offsuit = hand_notation.endswith('o')
+    
+    if suited:
+        return [r1 + 'h', r2 + 'h']
+    elif offsuit:
+        return [r1 + 'h', r2 + 'd']  # 違うスート
+    else:
+        return [r1 + 'h', r2 + 'd'] if r1 != r2 else [r1 + 'h', r1 + 'd']
